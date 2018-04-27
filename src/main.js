7f5d2c19b6311df6f984f84c14935195e070efa7
joe mckinney <mckinney.a.joe@gmail.com>
2018-04-27 16:25:54 -0700

    add alerts for when there are no responses in result from the input

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { API } from './logic.js';
 
 let displayResult1 = function(body) {
-debugger;
+// debugger;
   let practices = body.data;
+  console.log(practices.length);
   for ( let i=0; i<practices.length; i++) {
     $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
   }
@@ -27,32 +28,6 @@ $(document).ready(function() {
   $("#listDoctors").click(function() {
     let api = new API();
     api.makeCall(displayResult1, displayError1);
-
-    // let promise = new Promise(function(resolve, reject) {
-    //   let request = new XMLHttpRequest();
-    //   let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
-    //   request.onload = function() {
-    //     if (this.status === 200) {
-    //       resolve(request.response);
-    //     } else {
-    //       reject(Error(request.statusText));
-    //     }
-    //   };
-    //   request.open("GET", url, true);
-    //   request.send();
-    // });
-    //
-    // promise.then(function(response) {
-    //   const body = JSON.parse(response);
-    //   // console.log(body.data);
-    //   let practices = body.data;
-    //   for ( let i=0; i<practices.length; i++) {
-    //     $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
-    //   }
-    //   console.log(practices);
-    // }, function(error) {
-    //   $('#output').text(`There was an error loading your request: ${error.responseText}.  Please try again.`);
-    // });
   });
   $("#searchSymptom").submit(function(event) {
     event.preventDefault();
@@ -75,9 +50,12 @@ $(document).ready(function() {
       const body = JSON.parse(response);
       // console.log(body.data);
       let practices = body.data;
+      if (practices.length === 0) {
+        alert("Oops, that input didn't match any results.");
+      } else {
       for ( let i=0; i<practices.length; i++) {
         $('#bySymptomList').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
-        console.log(practices[i].profile.first_name);
+        }
       }
     }, function(error) {
       $('#bySymptomList').text(`There was an error loading your request: ${error.responseText}.  Please try again.`);
@@ -104,8 +82,13 @@ $(document).ready(function() {
       const body = JSON.parse(response);
       // console.log(body.data);
       let practi = body.data;
+      console.log(practi.length);
+      if (practi.length === 0){
+        alert("Oops, that input didn't match any results.");
+      } else {
       for ( let i=0; i<practi.length; i++) {
         $('#byNameList').append("<li>" + practi[i].profile.first_name + ", " + practi[i].profile.last_name +  " " + practi[i].profile.title + "; Address: "  + practi[i].practices[0].visit_address.street + ", " + practi[i].practices[0].visit_address.city + "Oregon;  Telephone: " + practi[i].practices[0].phones[0].number + "; Accepting New Patients: " + practi[i].practices[0].accepts_new_patients + "; " + practi[i].practices[0].website + ";" + "</li>");
+        }
         // console.log(practi[i].practices[0].website);
       }
     }, function(error) {
