76b3b3af68da3e146dd43cd440693e33b07d1700
joe mckinney <mckinney.a.joe@gmail.com>
2018-04-27 15:51:23 -0700

    update logic to seperate first api call from user interface logic, and clone project from github to make sure still works.

---
+++ src/logic.js
@@ -0,0 +1,27 @@
+class API {
+
+  makeCall(displayResult1, displayError1) {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      console.log(body.bikes);
+      displayResult1(body);
+    }, function(error) {
+      displayError1(error);
+    });
+  }
+}
+
+export { API };
--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { API } from './logic.js';
 
+let displayResult1 = function(body) {
+debugger;
+  let practices = body.data;
+  for ( let i=0; i<practices.length; i++) {
+    $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
+  }
+}
+let displayError1 = function(error) {
+  $("#output").text(`There was an error processing your request: ${error.message}`);
+}
 $(document).ready(function() {
   $("#inPortland").click(function() {
     $("#search-in-portland").toggle();
@@ -14,31 +25,34 @@ $(document).ready(function() {
     $("#search-by-name").toggle();
   });
   $("#listDoctors").click(function() {
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+    let api = new API();
+    api.makeCall(displayResult1, displayError1);
 
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      // console.log(body.data);
-      let practices = body.data;
-      for ( let i=0; i<practices.length; i++) {
-        $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
-      }
-      console.log(practices);
-    }, function(error) {
-      $('#output').text(`There was an error loading your request: ${error.responseText}.  Please try again.`);
-    });
+    // let promise = new Promise(function(resolve, reject) {
+    //   let request = new XMLHttpRequest();
+    //   let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
+    //   request.onload = function() {
+    //     if (this.status === 200) {
+    //       resolve(request.response);
+    //     } else {
+    //       reject(Error(request.statusText));
+    //     }
+    //   };
+    //   request.open("GET", url, true);
+    //   request.send();
+    // });
+    //
+    // promise.then(function(response) {
+    //   const body = JSON.parse(response);
+    //   // console.log(body.data);
+    //   let practices = body.data;
+    //   for ( let i=0; i<practices.length; i++) {
+    //     $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
+    //   }
+    //   console.log(practices);
+    // }, function(error) {
+    //   $('#output').text(`There was an error loading your request: ${error.responseText}.  Please try again.`);
+    // });
   });
   $("#searchSymptom").submit(function(event) {
     event.preventDefault();
