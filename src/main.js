1b07a68eea617bfdc68bec4afec7f1a0e36fc942
joe mckinney <mckinney.a.joe@gmail.com>
2018-04-27 13:29:11 -0700

    update project for searching for doctors name and returning information about each doctor.

--- src/main.js
+++ src/main.js
@@ -22,14 +22,42 @@ $(document).ready(function() {
     promise.then(function(response) {
       const body = JSON.parse(response);
       // console.log(body.data);
-      var practices = body.data;
+      let practices = body.data;
       for ( let i=0; i<practices.length; i++) {
         $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
-        // $('#output').append("<li>" + practices[i].profile.bio);
       }
       console.log(practices);
     }, function(error) {
       $('#output').text(`There was an error loading your request: ${error.responseText}.  Please try again.`);
+    });
+  });
+  $("#searchName").submit(function(event) {
+    event.preventDefault();
+    let doctorName = $("#doctorName").val();
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      // console.log(body.data);
+      let practi = body.data;
+      for ( let i=0; i<practi.length; i++) {
+        $('#byNameList').append("<li>" + practi[i].profile.first_name + ", " + practi[i].profile.last_name +  " " + practi[i].profile.title + "; Address: "  + practi[i].practices[0].visit_address.street + ", " + practi[i].practices[0].visit_address.city + "Oregon;  Telephone: " + practi[i].practices[0].phones[0].number + "; Accepting New Patients: " + practi[i].practices[0].accepts_new_patients + ";" + "</li>");
+        // console.log(practi[i].practices[0].accepts_new_patients);
+      }
+    }, function(error) {
+      $('#byNameList').text(`There was an error loading your request: ${error.responseText}.  Please try again.`);
     });
   });
 });
