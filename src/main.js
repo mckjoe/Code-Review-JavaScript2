3575d10e5c4e6bbb0840b9531f0748b4d7cc66d8
joe mckinney <mckinney.a.joe@gmail.com>
2018-04-27 11:14:32 -0700

    add key variable for project and limit doctors response to 25 for doctors in portland Oregon.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $("#listDoctors").click(function() {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = "https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=5a42f7e9bf2406d748b79384a67ff5fd";
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -23,10 +23,11 @@ $(document).ready(function() {
       const body = JSON.parse(response);
       // console.log(body.data);
       var practices = body.data;
-      console.log(practices);
       for ( let i=0; i<practices.length; i++) {
-        $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name + "</li>");
+        $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "</li>");
+        // $('#output').append("<li>" + practices[i].profile.bio);
       }
+      console.log(practices);
     }, function(error) {
       $('#output').text(`There was an error loading your request: ${error.responseText}.  Please try again.`);
     });
