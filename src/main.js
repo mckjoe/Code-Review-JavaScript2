a347b15c51f656c3d7075959b46224c66f828edb
joe mckinney <mckinney.a.joe@gmail.com>
2018-06-15 15:27:12 -0700

    fix forms to empty their values after a search, and update styling for some hover effects.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { API } from './logic.js';
 let displayResult1 = function(body) {
   let practices = body.data;
   for ( let i=0; i<practices.length; i++) {
-    $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "<br> <strong>Address:</strong> " + practices[i].practices[0].visit_address.street + " "  + practices[i].practices[0].visit_address.city + ", Oregon; <br> <strong>Phone: </strong>" + practices[i].practices[0].phones[0].number + ";<br> <strong>Accepting new patients: </strong>" + practices[i].practices[0].accepts_new_patients + "; <br> <strong>Website:</strong> " +  practices[i].practices[0].website + "</li>"    );
+    $('#output').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "<br> <strong>Address:</strong> " + practices[i].practices[0].visit_address.street + " "  + practices[i].practices[0].visit_address.city + ", Oregon <br> <strong>Phone: </strong>" + practices[i].practices[0].phones[0].number + "<br> <strong>Accepting new patients: </strong>" + practices[i].practices[0].accepts_new_patients + " <br> <strong>Website:</strong> " +  practices[i].practices[0].website + "</li>");
     }
 }
 let displayError1 = function(error) {
@@ -36,6 +36,7 @@ $(document).ready(function() {
   $("#searchSymptom").submit(function(event) {
     event.preventDefault();
     let symptomName = $("#symptomName").val();
+    $("#symptomName").val("");
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptomName}&location=or-portland&skip=0&limit=5&user_key=${process.env.API_KEY}`;
@@ -57,7 +58,7 @@ $(document).ready(function() {
         alert("Oops, that input didn't match any results.");
       } else {
       for ( let i=0; i<practices.length; i++) {
-        $('#bySymptomList').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "<br> <strong>Address:</strong> " + practices[i].practices[0].visit_address.street + " "  + practices[i].practices[0].visit_address.city + ", Oregon; <br> <strong>Phone: </strong>" + practices[i].practices[0].phones[0].number + ";<br> <strong>Accepting new patients: </strong>" + practices[i].practices[0].accepts_new_patients + "; <br> <strong>Website:</strong> " +  practices[i].practices[0].website + "</li>"    );
+        $('#bySymptomList').append("<li>" + practices[i].profile.first_name + ", " + practices[i].profile.last_name +  " " + practices[i].profile.title + "<br> <strong>Address:</strong> " + practices[i].practices[0].visit_address.street + " "  + practices[i].practices[0].visit_address.city + ", Oregon <br> <strong>Phone: </strong>" + practices[i].practices[0].phones[0].number + "<br> <strong>Accepting new patients: </strong>" + practices[i].practices[0].accepts_new_patients + " <br> <strong>Website:</strong> " +  practices[i].practices[0].website + "</li>"    );
         }
       }
     }, function(error) {
@@ -73,6 +74,7 @@ $(document).ready(function() {
   $("#searchName").submit(function(event) {
     event.preventDefault();
     let doctorName = $("#doctorName").val();
+    $("#doctorName").val("");
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=or-portland&skip=0&limit=25&user_key=${process.env.API_KEY}`;
@@ -95,7 +97,7 @@ $(document).ready(function() {
         alert("Oops, that input didn't match any results.");
       } else {
       for ( let i=0; i<practi.length; i++) {
-        $('#byNameList').append("<li>" + practi[i].profile.first_name + ", " + practi[i].profile.last_name +  " " + practi[i].profile.title + ";<br>  <strong>Address:</strong> "  + practi[i].practices[0].visit_address.street + ", " + practi[i].practices[0].visit_address.city + "Oregon; <br> <strong>Telephone:</strong> " + practi[i].practices[0].phones[0].number + "; <br> <strong>Accepting new patients:</strong> " + practi[i].practices[0].accepts_new_patients + "; <br> <strong>Website:</strong> " + practi[i].practices[0].website + ";" + "</li>");
+        $('#byNameList').append("<li>" + practi[i].profile.first_name + ", " + practi[i].profile.last_name +  " " + practi[i].profile.title + ";<br>  <strong>Address:</strong> "  + practi[i].practices[0].visit_address.street + ", " + practi[i].practices[0].visit_address.city + "Oregon <br> <strong>Telephone:</strong> " + practi[i].practices[0].phones[0].number + " <br> <strong>Accepting new patients:</strong> " + practi[i].practices[0].accepts_new_patients + " <br> <strong>Website:</strong> " + practi[i].practices[0].website + "</li>");
         }
       }
     }, function(error) {
